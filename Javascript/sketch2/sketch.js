8dfbdf2028c09d669a10e380d46dc9fad6dede56
LeL <jj20050919@gmail.com>
2023-03-07 15:24:47 -0600

    boxes
    woah

--- Javascript/sketch2/sketch.js
+++ Javascript/sketch2/sketch.js
@@ -5,10 +5,41 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let boxes = [];
+let boxLength = 10;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  noFill();
+  colorMode(HSB,255);
+  angleMode(DEGREES);
+  for (let x = boxLength; x < (windowWidth/boxLength - 1) * boxLength; x += boxLength) {
+    for (let y = boxLength; y < (windowHeight/boxLength - 1) * boxLength; y += boxLength) {
+      boxes.push(new Box(x,y,boxLength,random(-y/boxLength,y/boxLength)/5, color((x+y) % 255, 255,255)));
+    }
+  }
+  for (box of boxes) {
+    push();
+    translate(box.x, box.y);
+    rotate(box.rot);
+    stroke(box.c);
+    square(0, 0, box.l);
+    pop();
+  }
 }
 
 function draw() {
+
+
+}
+
+class Box {
+  constructor(x, y, l, r, c) {
+    this.x = x;
+    this.y = y;
+    this.l = l;
+    this.rot = r;
+    this.c = c;
+  }
 }
---
+++ index.md
@@ -0,0 +1,4 @@
+## Demos
+- [Physics Simulation](Javascript/physics%20sim)
+- [sketch 1](Javascript/sketch1)
+- [sketch 2](Javascriptsketch2)
