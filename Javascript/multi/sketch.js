e4dacc17dda1c7e7b8841e0813b9a7167a99213d
maaiv <jj20050919@gmail.com>
2023-03-20 22:13:57 -0600

    Cleaned up code
    reformatted a bunch of stuff and added some new comments to clarify function blocks

--- Javascript/multi/sketch.js
+++ Javascript/multi/sketch.js
@@ -1,51 +1,27 @@
-// Project Title
-// Your Name
-// Date
+// 3D Amongst Us
+// Johnson Ji
+// March 19th, 2023
+//
+// Controls: 
+// [WASD] to move
+// [SPACE] to jump
+// [1] to equip nothing
+// [2] to equip lamp
+// [3] to equip knife
+// [MOUSE] to look around (click the screen to lock the pointer) 
+// [CLICK] to kill other players when holding knife
+// [P] to enter debug mode
+// [UP and DOWN ARROWS] to change the world lighting
+//
+// Notes:
+// - Multiplayer only supports up to 5 players due to WEBGL limitations in rendering spotlights
 //
 // Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-// lol
-// Checklist:
-// 1. add make collisions work in all directions in the 2d plane || DONE took way too long
-//    - probably just recode my normal finder thingy from scratch
-// 2. add proper UI
-//    - main menu last
-//    - selected item
-//    - global chat bruh fml
-//    - name tags?
-//    - working minimap idk
-//      - would that give too much power?
-// 3. add method to kill crewmate || DONE
-//    - add ragdoll physics maybe
-//    - change sprite
-//    - impostor class
-//    - create red outline
-//    - I need to learn blender
-// 4. add true 3d collisions (maybe?) || DONE
-//    - not gonna work with the current lighting system
-//    - tbh I just wanna make this to see if I can
-//    - funny parkour challenge
-//    - assign y/height value to objects
-//    - if collide, check y intersection
-// 5. add SFX
-//    - bg music
-//    - kill sfx funny
-//    - among us trap remix
-//    - walking sfx
-// 6. spruce up guest movement
-//    - add velocity variables to shared (gravity too maybe)
-//    - move figure in between server updates
-// 7. clean up code || DONE
-//    - I am sorry mr schellenberg
-// 8. first person (maybe?)
-//    - adjust camera position
-//    - introduce flashlight lighting???
-//    - better immesion
-// 9. add ranged weaponary
-//    - probably that one gun thing
-//    - allows impostors to kill from afar, but gives away position through sound 
-// 10. venting (maybe?)
-//    - either classic fast travel or hidden passageways only accessable to the impostor
+// - 3D rendering with specular lighting and adaptive ambient colouring
+// - Custom 3D player models drawn in WEBGL
+// - Normal angle calculator for flexible 2D collisions with (almost) any shape
+// - Incorporated height and y position elements with p5.collide2D to create pseudo 3D collisions
+// - Multiplayer interactivity through p5.party
 
 
 // Instantiating variables
@@ -53,18 +29,12 @@ let camYaw = 0; //x
 let camPitch = 0; //y
 let camDistance = 300;
 let lightpos = [];
-let lightSize = 10;
-
-let playerAcceleration = 1;
-let playerDeceleration = 0.9;
-let maxVelocity = 6;
-  
 
 // Create environment objects
-let myTerrain = [
+let hostTerrain = [
   {type: 'box', x: 0, y: 50, z: 0, width: 3600, height: 100, length: 3600},
   {type: 'box', x: 300, y: -30, z: 0, width: 100, height: 60, length: 100}, 
-  {type: 'box', x: 450, y: -110, z: 0, width: 100, height: 60, length: 100}, 
+  {type: 'box', x: 500, y: -110, z: 0, width: 100, height: 60, length: 100}, 
   {type: 'box', x: 300, y: -30, z: 400, width: 300, height: 60, length: 300},
   {type: 'box', x: -300, y: -30, z: -600, width: 800, height: 60, length: 800},
   {type: 'cylinder', x: -300, y: -30, z: 400, radius: 100, height: 60},
@@ -72,22 +42,32 @@ let myTerrain = [
   {type: 'cylinder', x: -700, y: -380, z: 1500, radius: 200, height: 760},
   {type: 'cylinder', x: -700 * 2, y: -350 - 30*2, z: 1500, radius: 200, height: 700 + 60 * 2}
 ];
-
-
 for (let i = 1; i < 10; i += 1) {
-  myTerrain.push({type: 'box', x: 450, y: -110 - i * 80, z: i * 200, width: 100, height: 60, length: 100});
+  hostTerrain.push({type: 'box', x: 500, y: -110 - i * 80, z: i * 200, width: 100, height: 60, length: 100});
 }
 
-// Connect to the server and shared data
+// Connect to the server and shared data, and load sounds
 function preload() {
   partyConnect("wss://demoserver.p5party.org", "among");
   my = partyLoadMyShared();
   guests = partyLoadGuestShareds();
-  shared = partyLoadShared("shared", {ambientLevel: 0, debugState: false, terrain: myTerrain});
+  shared = partyLoadShared("shared", {
+                                      ambientLevel: 0, 
+                                      debugState: false, 
+                                      terrain: hostTerrain, 
+                                      lightSize: 10,
+                                      playerAcceleration: 1,
+                                      playerDeceleration: 0.9,
+                                      playerMaxVelocity: 6,
+                                      playerJumpPower: 5,
+                                      worldGravity: 0.15,
+                                      playerPerspective: 3
+                                    });
   killSFX = loadSound('assets/killSFX.mp3');
+  partySubscribe("die", die);
 };
 
-// Set sketch modes, canvas, and 
+// Set sketch modes, canvas, and subscribe to die message
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
@@ -104,14 +84,11 @@ function setup() {
   console.log("me", JSON.stringify(my));
   console.log("guests", JSON.stringify(guests));
   console.log("am i host?", partyIsHost());
-
-  partySubscribe("die", die);
   } 
 
 
-
+// Game update loop
 function draw() {
-
   drawInit();
 
   collideVisual();
@@ -125,39 +102,28 @@ function draw() {
   drawPlayers();
 
   drawEnvironment();
-
-
-
 }
 
-// Set background and pointer lock
+// Set background and lock pointer light
 function drawInit() {
   background(0);
  
-  // create a weak light so WEBGL doesn't just break when there are no lights
-  pointLight(
-    0,0,0,
-    0,-300,0
-  );
+  // create a global light so WEBGL doesn't just break when there are no lights
   pointLight(
     0,0,shared.ambientLevel,
     0,-1900,0
   );
-
   ambientLight(shared.ambientLevel/2);
 
   if (mouseIsPressed) {
     requestPointerLock();
   }
-
-
 }
 
 // Creates a visualizer for the different hitboxes and colliders above the player if in debug mode
 function collideVisual() {
   if (shared.debugState) {
     collideVisualCanvas.background(255);
-
 
     // draw environment hitboxes
     for (let terrainObject of shared.terrain) {
@@ -178,15 +144,13 @@ function collideVisual() {
     // draw character hitboxes
     for (let guest of guests) {
       collideVisualCanvas.circle(guest.player.x/10 + 90,guest.player.z/10 + 90, 6);
-      collideVisualCanvas.circle(guest.player.x/10 + 90 + sin(guest.player.dir) * 3, guest.player.z/10 + 90 + cos(guest.player.dir) * 3, 2)
-
+      collideVisualCanvas.circle(guest.player.x/10 + 90 + sin(guest.player.dir) * 3, guest.player.z/10 + 90 + cos(guest.player.dir) * 3, 2);
     }
 
     //draw visualizer above the player model
     push();
-      translate(my.player.x - 50,my.player.y-65 - 100,my.player.z)
+      translate(my.player.x - 50,my.player.y-65 - 100,my.player.z);
       image(collideVisualCanvas,0,0,100,100);
-
     pop();
   }
 }
@@ -198,8 +162,7 @@ function updateMyPlayer() {
 
 // Move camera
 function updateCam() {
-
-  if (my.player.perspective === 3) {
+  if (shared.playerPerspective === 3) {
     cam.setPosition(
       my.player.x + cos(camYaw) * camDistance * cos(camPitch),
       my.player.y - 60 + sin(camPitch) * camDistance,
@@ -207,14 +170,22 @@ function updateCam() {
       );
     camYaw += movedX/10;
     camPitch -= movedY/10;
+
+    // prevent camera inversion
+    if (camPitch > 89) {
+      camPitch = 89;
+    }
+    else if (camPitch < -89) {
+      camPitch = -89;
+    }
   
     cam.lookAt(my.player.x,my.player.y - 60,my.player.z);
   }
-  else if (my.player.perspective === 1) {
+  else if (shared.playerPerspective === 1) {
     cam.setPosition(
-      my.player.x + cos(camYaw) * 1 * cos(camPitch),
-      my.player.y - 60 + sin(camPitch) * 1,
-      my.player.z + sin(camYaw) * 1 * cos(camPitch),
+      my.player.x + cos(camYaw) * cos(camPitch),
+      my.player.y - 60 + sin(camPitch),
+      my.player.z + sin(camYaw) * cos(camPitch),
       );
     camYaw += movedX/10;
     camPitch -= movedY/10;
@@ -223,7 +194,7 @@ function updateCam() {
     }
 }
 
-// Create and store position of each player light
+// Create and store xy coordinate of each player light
 function createLights() {
   lightpos = [];
 
@@ -233,23 +204,21 @@ function createLights() {
         0,0,255,
         guest.player.x,guest.player.y - 400,guest.player.z,
         0,1,0,
-        85,40/lightSize);
+        85,40/shared.lightSize);
       lightpos.push([guest.player.x,guest.player.z]);
     }
   }
 }
 
-
-
 // Calculate ambient lighting and draw player models
 function drawPlayers() {
   for (let guest of guests) {
-    if (guest.player != my.player || my.player.perspective === 3) {
+    if (guest.player != my.player || shared.playerPerspective === 3) {
       push();
         // calculate ambient lighting depending on the closest distance to another light before rendering
         let minimumDistance = min(lightpos.map(v => dist(guest.player.x,guest.player.z,v[0], v[1])));
-        ambientLight(map(minimumDistance,0,125 + 50*lightSize,105,5,true));
-        drawCrewMateModel(guest.player.x,guest.player.y,guest.player.z,guest.player.dir,guest.player.h,guest.player.hold,guest.player.alive)
+        ambientLight(map(minimumDistance,0,125 + 50*shared.lightSize,105,5,true));
+        drawCrewMateModel(guest.player.x,guest.player.y,guest.player.z,guest.player.dir,guest.player.h,guest.player.hold,guest.player.alive);
       pop();
     }
   }
@@ -258,7 +227,7 @@ function drawPlayers() {
   if (shared.debugState) {
     push();
       let minimumDistance = min(lightpos.map(v => dist(0,0,v[0], v[1])));
-      ambientLight(map(minimumDistance,0,125 + 50*lightSize,105,5,true));
+      ambientLight(map(minimumDistance,0,125 + 50*shared.lightSize,105,5,true));
       drawCrewMateModel(0,0,0,0,180,2,false);
     pop();
   }
@@ -323,7 +292,7 @@ function drawCrewMateModel(x,y,z,dir,h,hold,alive) {
   else {
     // draw main body
     push();
-      translate(0,15,0)
+      translate(0,15,0);
       ellipsoid(20,15,15);
       scale(1, 1, 0.75);
       translate(0,-9,0)
@@ -341,10 +310,10 @@ function drawCrewMateModel(x,y,z,dir,h,hold,alive) {
       specularMaterial(30000);
       shininess(100);
       ambientMaterial(0,0,255);
-      translate(0,-5,0)
+      translate(0,-5,0);
       cylinder(4,20);
 
-      translate(3,-9,0)
+      translate(3,-9,0);
       sphere(5);
       translate(-6,0,0);
       sphere(5);
@@ -355,8 +324,6 @@ function drawCrewMateModel(x,y,z,dir,h,hold,alive) {
       translate(0,8,-14);
       box(24,20,10);
     pop();
-    
-
   }
   pop();
 }
@@ -366,6 +333,9 @@ function drawEnvironment() {
 
   for (let terrainObject of shared.terrain) {
     push();
+      if (!shared.debugState) {
+        noStroke();
+      }
       translate(terrainObject.x,terrainObject.y,terrainObject.z);
       if (terrainObject.type === 'box') {
         box(terrainObject.width,terrainObject.height,terrainObject.length);
@@ -389,6 +359,7 @@ function drawEnvironment() {
   }
 }
 
+// Check if a player is intersecting with the terrain
 function checkCollisions(playerX,playerY,playerZ,terrainObject) {
   if (terrainObject.type === 'box') {
     if (collideRectCircle(
@@ -400,9 +371,7 @@ function checkCollisions(playerX,playerY,playerZ,terrainObject) {
       playerZ,
       60
     )) {
-      if (playerY > terrainObject.y - terrainObject.height/2 && playerY - 65 < terrainObject.y + terrainObject.height/2) {
-        return(true);
-      }
+      return(playerY > terrainObject.y - terrainObject.height/2 && playerY - 65 < terrainObject.y + terrainObject.height/2);
     }
   }
   else if (terrainObject.type === 'cylinder') {
@@ -414,18 +383,19 @@ function checkCollisions(playerX,playerY,playerZ,terrainObject) {
       playerZ,
       60
     )) {
-      if (playerY > terrainObject.y - terrainObject.height/2 && playerY - 65 < terrainObject.y + terrainObject.height/2) {
-        return(true);
-      }
+    return(playerY > terrainObject.y - terrainObject.height/2 && playerY - 65 < terrainObject.y + terrainObject.height/2);
     }
   }
 return(false);
 }
 
-
+// Calculate the normal angle between the player and the collided object
 function findNormal(playerX,playerZ,playerDir,terrainObject) {
+  let normalCollide;
+
+  // set collide type
   if (terrainObject.type === 'box') {
-    var normalCollide = (x, z, dir, terrainObject) => collidePointRect(
+    normalCollide = (x, z, dir, terrainObject) => collidePointRect(
       x + sin(dir) * 40,
       z + cos(dir) * 40,
       terrainObject.x - terrainObject.width/2, 
@@ -435,7 +405,7 @@ function findNormal(playerX,playerZ,playerDir,terrainObject) {
     );
   }
   else if (terrainObject.type === 'cylinder') {
-    var normalCollide = (x, z, dir, terrainObject) => collidePointCircle(
+    normalCollide = (x, z, dir, terrainObject) => collidePointCircle(
       x + sin(dir) * 40,
       z + cos(dir) * 40,
       terrainObject.x, 
@@ -448,11 +418,14 @@ function findNormal(playerX,playerZ,playerDir,terrainObject) {
   let dir1;
   let dir2;
   if (normalCollide(playerX,playerZ,tempDir,terrainObject)) {
+
+    // find right-most point
     while (normalCollide(playerX,playerZ,tempDir,terrainObject)) {
       tempDir += 1;
     }
-
     dir1 = tempDir;
+
+    // find left-most point
     tempDir = playerDir;
     while (normalCollide(playerX,playerZ,tempDir,terrainObject)) {
       tempDir -= 1;
@@ -461,11 +434,13 @@ function findNormal(playerX,playerZ,playerDir,terrainObject) {
     }
 
   else {
+    // find right-most point
     while (!normalCollide(playerX,playerZ,tempDir,terrainObject)) {
       tempDir += 1;
     }
-  
     dir1 = tempDir;
+
+    // find left-most point
     tempDir = playerDir;
     while (!normalCollide(playerX,playerZ,tempDir,terrainObject)) {
       tempDir -= 1;
@@ -476,12 +451,10 @@ function findNormal(playerX,playerZ,playerDir,terrainObject) {
   return(round((dir1 + dir2)/2,2));
 }
 
+// Update player if killed
 function die(data) {
-  console.log(my.player.id, data.id)
   if (data.id === my.player.id) {
     killSFX.play();
-    
-    console.log('bruh');
     my.player.alive = false;
     my.player.dx = data.dx;
     my.player.dy = data.dy;
@@ -490,6 +463,7 @@ function die(data) {
   }
 }
 
+// Toggle debug mode
 function keyPressed() {
   if (keyCode === 80) {
     shared.debugState = !shared.debugState;
@@ -510,11 +484,13 @@ class Crewmate {
     this.hold = 1;
     this.alive = true;
     this.id = noise(random(1,10));
-    this.perspective = 3;
+
   }
 
   update() {
+    // player state if alive
     if (this.alive) {
+
       // select item
       if (keyIsDown(49)) {
         this.hold = 0;
@@ -526,26 +502,23 @@ class Crewmate {
         this.hold = 2;
       }
 
+      // use knife
       if (this.hold === 2) {
         for (let guest of guests) {
-          if (guest.player != my.player) {
+          if (guest.player != my.player && mouseIsPressed && !killSFX.isPlaying()) {
             if (dist(guest.player.x,guest.player.y,guest.player.z,my.player.x,my.player.y,my.player.z) < 300) {
-              if (mouseIsPressed && !killSFX.isPlaying()) {
-                killSFX.play();
-                let dir = atan2(my.player.x - guest.player.x, my.player.z - guest.player.z)
-                partyEmit("die", {
-                  id: guest.player.id,
-                  dx: sin(dir) * -5,
-                  dy: -4,
-                  dz: cos(dir) * -5
-                  })
-                
-              }
+              killSFX.play();
+              let tempDir = atan2(my.player.x - guest.player.x, my.player.z - guest.player.z);
+              partyEmit("die", {
+                id: guest.player.id,
+                dx: sin(tempDir) * -5,
+                dy: -4,
+                dz: cos(tempDir) * -5
+              });
             }
           }
         }
       }
-
 
       // apply x velocity and check collisions
       this.x += this.dx;
@@ -562,11 +535,10 @@ class Crewmate {
       }
 
       // point in direction of motion
-
-      if (this.perspective === 3) {
+      if (shared.playerPerspective === 3) {
         this.dir = atan2(this.dx,this.dz);
       }
-      else if (this.perspective === 1) {
+      else if (shared.playerPerspective === 1) {
         this.dir = -camYaw - 90;
       }
 
@@ -583,7 +555,7 @@ class Crewmate {
             }
             this.dy = 0;
             if (keyIsDown(32)) {
-              this.dy = -5;
+              this.dy = -shared.playerJumpPower;
             }
             
           }
@@ -597,11 +569,10 @@ class Crewmate {
         } 
       }
 
+      // apply gravity
       if (!touchingGround) {
-        this.dy += 0.15;
+        this.dy += shared.worldGravity;
       }
-
-
 
       // detect keyboard input
         // 87 = W
@@ -610,7 +581,7 @@ class Crewmate {
         // 68 = D
       if (keyIsDown(83) + keyIsDown(87) === 1 || keyIsDown(65) + keyIsDown(68) === 1) {
 
-        // perform math stuff
+        // perform math stuff to condense movement controls into a "single line" (this was a terrible idea)
         let magnitude = !keyIsDown(83) * 2 - 1;
         let dir = 
         (keyIsDown(68) * 0 + 
@@ -620,30 +591,32 @@ class Crewmate {
         (keyIsDown(68) + 
         keyIsDown(87) + 
         keyIsDown(65) + 
-        keyIsDown(83))  * magnitude
+        keyIsDown(83)) * magnitude;
 
         // accelerate player
-        this.dx -= playerAcceleration * sin(dir - camYaw);
-        this.dz -= playerAcceleration * cos(dir - camYaw);
+        this.dx -= shared.playerAcceleration * sin(dir - camYaw);
+        this.dz -= shared.playerAcceleration * cos(dir - camYaw);
 
         // cap player velocity
-        if (Math.sqrt(this.dx**2 + this.dz**2) > maxVelocity) {
-          let ratio = maxVelocity/Math.sqrt(this.dx**2 + this.dz**2);
+        if (Math.sqrt(this.dx**2 + this.dz**2) > shared.playerMaxVelocity) {
+          let ratio = shared.playerMaxVelocity/Math.sqrt(this.dx**2 + this.dz**2);
           this.dx = lerp(0,this.dx,ratio);
           this.dz = lerp(0,this.dz,ratio);
         }
-
       }
       else {
         // decelerate player
-        this.dx = this.dx * playerDeceleration
-        this.dz = this.dz * playerDeceleration
+        this.dx = this.dx * shared.playerDeceleration
+        this.dz = this.dz * shared.playerDeceleration
       }
+      
+      // change world ambient level
       shared.ambientLevel += keyIsDown(UP_ARROW) - keyIsDown(DOWN_ARROW);
-
-
     }
+
+    // player state if not alive
     else {
+
       // apply x velocity and check collisions
       this.x += this.dx;
       this.z += this.dz;
@@ -664,7 +637,6 @@ class Crewmate {
 
       // apply y velocity and check collisions
       this.y += this.dy;
-
       let touchingGround = false;
 
       for (let terrainObject of shared.terrain) {
@@ -679,16 +651,16 @@ class Crewmate {
               this.y += 0.1;
             }
           }
+
           this.dy = -this.dy;
           touchingGround = true;
         }
       }
 
-      // this.dx = this.dx * playerDeceleration
-      // this.dz = this.dz * playerDeceleration
+      // apply gravity
       if (!touchingGround) {
-        this.dy += 0.15;
+        this.dy += shared.worldGravity;
       }
-    } //else end
+    }
   }
 }
