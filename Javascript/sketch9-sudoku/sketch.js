69398f6fb2e6538113f81bee3893298cc33b0490
LeL <jj20050919@gmail.com>
2023-03-30 15:19:59 -0600

    started working on sudoku solver
    using logic and recurrsion

--- Javascript/sketch9-sudoku/sketch.js
+++ Javascript/sketch9-sudoku/sketch.js
@@ -23,7 +23,7 @@ let grid = [
   [0, 0, 4, 0, 0, 0, 1, 0, 0],
   [0, 9, 0, 0, 0, 0, 0, 0, 0],
   [5, 0, 0, 0, 0, 0, 0, 7, 3],
-  [0, 0, 2, 0, 0, 0, 0, 0, 0],
+  [0, 0, 2, 0, 1, 0, 0, 0, 0],
   [0, 0, 0, 0, 4, 0, 0, 0, 9],
 ];
 
@@ -31,14 +31,12 @@ let grid = [
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  createRandomGrid();
-  noStroke();
+  textAlign(CENTER, CENTER);
+
 }
 
 function draw() {
-  if (keyIsDown(32)) {
-    grid = createRandomGrid(ROWS, COLS);
-  }
+
   resizeCanvas(windowWidth,windowHeight);
   if (width < height) { 
     cellSize = width/ROWS;
@@ -53,28 +51,18 @@ function draw() {
 function displayGrid() {
   for (let y = 0; y < grid.length; y += 1) {
     for (let x = 0; x < grid[y].length; x += 1) {
-      if (grid[y][x] !== 0) {
-        text(grid[y][x], x * cellSize, y * cellSize, cellSize, cellSize);
-      }
       square(x * cellSize, y * cellSize, cellSize);
+      if (grid[y][x] !== 0) {
+        push();
+
+        fill("black");
+        textSize(cellSize * 0.6);
+        text(String(grid[y][x]), x * cellSize, y * cellSize + cellSize/20,  cellSize,  cellSize);
+        pop();
+      }
       
     }
   }
 }
 
-function mousePressed() {
-  let x = Math.floor(mouseX/cellSize);
-  let y = Math.floor(mouseY/cellSize);
-  grid[y][x] = !grid[y][x];
-}
 
-function createRandomGrid(ROWS, COLS) {
-  let newGrid = [];
-  for (let i = 0; i < ROWS; i++) {
-    newGrid.push([]);
-    for (let j = 0; j < COLS; j++) {
-      newGrid[i].push(round(random(0,2)));
-    }
-  }
-  return newGrid;
-}
